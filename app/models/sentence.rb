417e0027d16cbd4c8a08012dcd91653a3a4fc985
Krystan Menne and Mike Mahoney <kmenne@gmail.com>
2016-03-16 15:21:14 -0700

    disallow users from updating any but the last sentence in a story

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -2,4 +2,13 @@ class Sentence < ActiveRecord::Base
   belongs_to :story
   validates :author, presence: true
   validates_length_of :content, :maximum => 300, :allow_blank => false
+  validate :sentence_must_be_the_last_sentence, on: :update
+
+  private
+
+  def sentence_must_be_the_last_sentence
+    if self.story && (self != self.story.sentences.last)
+      errors.add(:story, ': Sentence must be the last sentence in the story.')
+    end
+  end
 end
--- spec/features/edit_sentence_pages_spec.rb
+++ spec/features/edit_sentence_pages_spec.rb
@@ -4,11 +4,25 @@ require 'story_helper'
 describe 'the edit a sentence path' do
   it 'allows a user to edit a sentence' do
     story = create_test_story
+    sentence = story.sentences.first
     visit story_path(story)
-    click_on 'Edit'
+    click_on "edit-sentence-#{sentence.id}"
     fill_in 'Sentence', with: 'I am an updated test sentence.'
     click_on 'Update Sentence'
     expect(page).not_to have_content('Test content.')
     expect(page).to have_content('I am an updated test sentence.')
+  end
+
+  it 'only allows the most recent sentence to be edited' do
+    story = create_test_story
+    first_sentence = story.sentences.first
+    second_sentence = story.sentences.create(content: 'Second sentence.',
+                                             author: 'Second author.')
+    visit story_path(story)
+    expect(page).not_to have_selector "#edit-sentence-#{first_sentence.id}"
+    visit edit_story_sentence_path(story, first_sentence)
+    fill_in 'Sentence', with: 'I am an updated test sentence.'
+    click_on 'Update Sentence'
+    expect(page).to have_content('errors')
   end
 end
