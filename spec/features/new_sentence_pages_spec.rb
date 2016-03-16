7166e776435ae2d6bcb1f7b34350712b2b04cd53
Krystan Menne and Mike Mahoney <mahonmr@me.com>
2016-03-16 14:27:07 -0700

    add test for sentence errors

--- spec/features/new_sentence_pages_spec.rb
+++ spec/features/new_sentence_pages_spec.rb
@@ -2,13 +2,21 @@ require "rails_helper"
 require 'story_helper'
 
 describe 'the add a new sentence path' do
-  it 'adds a new sentence to an existing story' do
+  before do
     test_story = create_test_story
     visit story_path(test_story)
     click_on 'Add a Sentence'
+  end
+
+  it 'adds a new sentence to an existing story' do
     fill_in 'Sentence', with: 'I am a new test sentence.'
     fill_in 'Author', with: 'New Test Author'
     click_on 'Create Sentence'
     expect(page).to have_content('I am a new test sentence.')
+  end
+
+  it 'gives an error if the sentence is blank' do
+    click_on 'Create Sentence'
+    expect(page).to have_content('errors')
   end
 end
