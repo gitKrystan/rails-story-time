4b61ad12f3cf5ba100572259ed11b2c63a4e99d3
Krystan Menne and Mike Mahoney <kmenne@gmail.com>
2016-03-16 09:46:33 -0700

    add content length validation

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,4 +1,5 @@
 class Sentence < ActiveRecord::Base
   belongs_to :story
   validates :author, presence: true
+  validates_length_of :content, :maximum => 300, :allow_blank => false
 end
--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -3,4 +3,5 @@ require "rails_helper"
 describe Sentence do
   it { should belong_to :story }
   it { should validate_presence_of :author }
+  it { should validate_length_of :content }
 end
