0f36b3d7cbf6426a3acb59eac46a8163bb0ac737
Krystan Menne and Mike Mahoney <mahonmr@me.com>
2016-03-17 08:53:47 -0700

    add tests to verify proper destruction of sentences

--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -1,8 +1,19 @@
 require "rails_helper"
+require "story_helper"
 
 describe Sentence do
   it { should belong_to :story }
   it { should belong_to :image }
   it { should validate_presence_of :author }
   it { should validate_length_of :content }
+
+  context '#destroy' do
+    it 'will only directly destroy the last sentence' do
+      story = create_test_story
+      first_sentence = story.sentences.first
+      second_sentence = story.sentences.create(content: 'Second sentence.',
+                                               author: 'Second author.')
+      expect(first_sentence.destroy).to be false
+    end
+  end
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,7 +1,18 @@
 require "rails_helper"
+require "story_helper"
 
 describe Story do
   it { should have_many(:sentences).dependent :destroy }
   it { should accept_nested_attributes_for :sentences }
   it { should validate_presence_of :title }
+
+  context '#destroy' do
+    it 'will delete all of its sentences' do
+      story = create_test_story
+      first_sentence = story.sentences.first
+      second_sentence = story.sentences.create(content: 'Second sentence.',
+                                               author: 'Second author.')
+      expect(story.destroy).to be_truthy
+    end
+  end
 end
